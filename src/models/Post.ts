c4a379ab9adaa04af3c4ddeb8a3c52243754ff50
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-22 14:20:15 +0530

    Created the Post Model Schema

---
+++ src/models/Post.ts
@@ -0,0 +1,40 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IPost extends Document{
+    title: String;
+    description: String;
+    hoursCount: Number
+    lecturerName: String;
+    tags: Array<String>;
+};
+
+const PostSchema = new Schema(
+    {
+        title:{
+            type:String,
+            required: true,
+        },
+
+        description: {
+            type:String,
+            required:true
+        },
+
+        hoursCount: {
+            type: Number,
+            required: true
+        },
+
+        lecturerName: {
+            type: String,
+            required: true
+        },
+
+        tags: {
+            type: Array,
+            required: true
+        }
+    }
+);
+
+export const Post = model<IPost>("Post", PostSchema);
