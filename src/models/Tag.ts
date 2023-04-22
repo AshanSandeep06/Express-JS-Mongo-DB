ec5542cb774d5a17cabecb1164c68116a3729d3a
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-22 14:24:59 +0530

    Content Updated

--- src/models/Tag.ts
+++ src/models/Tag.ts
@@ -1,5 +1,14 @@
-import { Schema } from "mongoose";
+import { Schema, Document, model } from "mongoose";
+
+export interface ITag extends Document {
+    text: String;
+}
 
 const TagSchema = new Schema({
-    
-});
+    text: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+export const Tag = model<ITag>("Tag", TagSchema);
