0841129d0b145a089d61e5f678cc5cc58937551e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-29 10:52:05 +0530

    Created API methods in CategoryController

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -1,19 +1,72 @@
 import { RequestHandler, Request, Response } from "express";
+import { Category } from "../models/Category";
 
 export default class CategoryController {
     createCategory: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
-        return res;
+        try{
+            let newCategory = new Category(req.body);
+            let response = await newCategory.save();
+
+            return res.status(200).json({message: "New Category Added", response: response});
+
+        }catch (error: unknown) {
+            if(error instanceof Error){
+                return res.status(500).json({message: error.message});
+            }else{
+                return res.status(500).json({message: "Unknown Error Occured..!"});
+            }
+        }
     };
 
     retrieveAllCategories: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
-        return res;
+        try{
+            let allCategories = await Category.find();
+
+            return res.status(200).json({message: "Loaded All Categroies", response: allCategories});
+        }catch(error: unknown){
+            if(error instanceof Error){
+                return res.status(500).json({message: error.message});
+            }else{
+                return res.status(500).json({message: "Unknown Error Occured..!"});
+            }
+        }
     };
 
     updateCategory: RequestHandler = async(req: Request, res: Response): Promise<Response> => {
-        return res;
+        try{
+            //Desctructuring assignment
+            const { id } = req.params;
+
+            let updatedCategory = await Category.findByIdAndUpdate(id, req.body, {new: true});
+
+            return res.status(200).json({message: "Category has been Updated..!", response: updatedCategory});
+
+        }catch(error: unknown) {
+            if(error instanceof Error){
+                return res.status(500).json({message: error.message});
+            }else{
+                return res.status(500).json({message: "Unknown Error Occured..!"});
+            }
+        }
     };
 
     deleteCtaegory: RequestHandler = async(req: Request, res: Response):Promise<Response> => {
-        return res;
+        try{
+            const { id } = req.params;
+            let deletedCategory = await Category.findByIdAndDelete(id);
+
+            if(!deletedCategory){
+                throw new Error("Something Went wrong");
+            }
+
+            return res.status(200).json({message: "Category has been Deleted..!", response: deletedCategory});
+
+        }catch(error: unknown){
+            if(error instanceof Error){
+                return res.status(500).json({message: error.message});
+            }else{
+                return res.status(500).json({message: "Unknown Error Occured..!"});
+            }
+        }
     }
 }
