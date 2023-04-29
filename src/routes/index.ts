ec6df4768b3c9cbfd9680a844410dc359e4ff9aa
AshanSandeep06 <ashansandeep06@gmail.com>
2023-04-29 09:43:00 +0530

    Content Updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import PostRoutes from "./PostRoutes";
+import CategoryRoutes from "./CategoryRoutes";
 
 const router: Router = Router();
 
@@ -8,5 +9,6 @@ const url_prefix = "/api/v1";
 // Methana router ekk use kre => /api/v1/post/? kyna path eka yatathe mkkhri path ekk match wenwwda kyla
 // Baleema sadaha.
 router.use(`${url_prefix}/post`, new PostRoutes().getRouter);
+router.use(`${url_prefix}/category`, new CategoryRoutes().getRouter);
 
 export default router;
